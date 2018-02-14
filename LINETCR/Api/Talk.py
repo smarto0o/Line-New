8f2d432b16394791dedbf8dc9639a43bb8ccc732
SELFBOT-MAN-PC <34406024+Selfbotman@users.noreply.github.com>
2018-02-14 10:51:11 +0000

    Update Talk.py

--- LINETCR/Api/Talk.py
+++ LINETCR/Api/Talk.py
@@ -19,10 +19,11 @@ class Talk:
   wait_for_mobile_path = "/Q";
   host = "gd2.line.naver.jp";
   port = 443;
-
-  UA = "Line/6.0.0 iPad4,1 9.0.2"
-  LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
-
+  #SELFBOT_MAN_PC
+  UA = "Line/8.0.1"
+  LA = "DESKTOPMAC\t10.10.2-YOSEMITE-x64\tMAC 4.5.0"
+  #UA = "Line/6.0.0 iPad4,1 9.0.2"
+  #LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
   authToken = None
   cert = None
 
